820267dcb0cd151356c0c91e3b66f917b3dc4793
clovis <clovis-@live.com>
2017-07-24 23:04:59 -0400

    Adds Effects

---
+++ Effects/android/MyAndroidLabelFontEffect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.Graphics;
+using Android.Widget;
+using Effects.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("XamarinWorkShopINTEC")]
+[assembly: ExportEffect(typeof(MyAndroidLabelFontEffect),"FontEffect")]
+namespace Effects.Droid
+{
+   public class MyAndroidLabelFontEffect: PlatformEffect
+    {
+        Typeface oldFont;
+
+        protected override void OnAttached()
+        {
+            if (Element is Label == false)
+            {
+                return;
+            }
+
+            var label = (TextView)Control;
+            oldFont = label.Typeface;
+            label.Typeface = Typeface.CreateFromAsset(Android.App.Application.Context.Assets, "Ancherr.ttf");
+        }
+        protected override void OnDetached()
+        {
+            if(oldFont != null)
+            {
+                var label = (TextView)Control;
+                label.Typeface = oldFont;
+            }
+        }
+    }
+}
---
+++ Effects/portable/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Effects
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            myLabel.Effects.Add(new LabelFontEffect());
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await System.Threading.Tasks.Task.Delay(3000);
+            myLabel.Effects.Clear();
+        }
+    }
+}
